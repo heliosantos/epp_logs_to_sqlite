21b24756e05c5fdc6f1ebd1c71f4c7c8927c719a
Hélio Santos <heliosantos99@gmail.com>
2020-01-26 22:31:00 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='epp_logs_to_sqlite',
+    version='1.0',
+    description='Convert egg plant performance test results parser',
+    packages=find_packages(),
+    install_requires=[
+        'epp_event_log_reader>=1.0,<2.0',
+    ],
+)
